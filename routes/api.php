76041cfb9cf84ef88a620bc9800959534d83e45c
thanhdo21d <thanhdo9xi@gmail.com>
2024-04-18 15:30:52 +0700

    note api

--- routes/api.php
+++ routes/api.php
@@ -20,23 +20,70 @@ use App\Http\Middleware\VerifyTokenMiddleware;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+// form login email , password
 Route::post('login', [LoginController::class, 'login']);
+
+// api register
+// {
+//   "name": "Jhon Doe",
+//   "email": "jhon@example.com",
+//   "password": "12345678",
+//   "password_confirmation": "12345678"
+// }
 Route::post('register', [RegisterController::class, 'register']);
 Route::get('/get-all/questions', [QuestionController::class, 'getAllQuestion']);
+
+// api/choose-question/id
+// body : {"idUser": 456,
+//     "user_answer": "your_answer_here"
+// }
+//
+
 Route::post('/choose-question/{id}', [QuestionController::class, 'checkUserChoose']);
+
+// idUser
 Route::get('/get-all-question-used-user/{id}', [QuestionController::class, 'getAlldetailsUserChoose']);
+
+// id từ bảng Details_User_Choose
 Route::get('/get-id-question-used-user/{id}', [QuestionController::class, 'getIdDetailsUserChoose']);
+
+//  'name' ,
+            // 'email' ,
+            // 'new_password'',
 Route::post('update-password', [UserController::class, 'resetPassword']);
 
+//id question
 Route::get('/questions/{id}', [QuestionController::class, 'getIdQuestion']);
 Route::middleware('auth:api')->group(function () {
     Route::get('profile', [ProfileController::class, 'show']);
     Route::post('logout', [ProfileController::class, 'logout']);
+
+    // name ,email
     Route::put('profile/update/{id}', [UserController::class, 'updateProfile']);
 
+    //id user ,  role
     Route::put('/users/{id}/role', [UserController::class, 'updateRole'])->middleware(VerifyTokenMiddleware::class);
+
+    //  'question' => 'required|string',
+    //         'image' => 'nullable|string',
+    //         'options' => 'required|string',
+    //         'choose' => 'required|array',
+    //         'answer' => 'required|string',
+    //         'point' => 'required|integer',
+    // body của choose [{"q":"22"},{"q":"22"},{"q":"22"},{"q":"22"}]
+
     Route::post('/create-question', [QuestionController::class, 'createQuestion'])->middleware(VerifyTokenMiddleware::class);
+
+    // id của question
     Route::delete('/delete-questions/{id}', [QuestionController::class, 'deleteQuestion'])->middleware(VerifyTokenMiddleware::class);
+
+    //id
+    // //  'question' => 'required|string',
+    //         'image' => 'nullable|string',
+    //         'options' => 'required|string',
+    //         'choose' => 'required|array',
+    //         'answer' => 'required|string',
+    //         'point' => 'required|integer',
+    // body của choose [{"q":"22"},{"q":"22"},{"q":"22"},{"q":"22"}]
     Route::put('/edit-questions/{id}', [QuestionController::class, 'updateQuestion'])->middleware(VerifyTokenMiddleware::class);
 });
